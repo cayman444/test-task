620394fc3adb219c80fcb02235ba6736df532dc3
cayman444 <artursavinX@yandex.ru>
2025-11-02 13:58:54 +0300

    feat: add adaptive size for the details product

--- src/features/product-details/components/Product.tsx
+++ src/features/product-details/components/Product.tsx
@@ -33,8 +33,8 @@ export const Product = () => {
           {productDetails.name}
         </h1>
       </div>
-      <div className="flex gap-4">
-        <div className="relative w-100 h-100">
+      <div className="flex gap-4 flex-col md:flex-row">
+        <div className="relative w-full h-100 md:w-100">
           <img
             src={productDetails.image}
             alt={productDetails.type}
