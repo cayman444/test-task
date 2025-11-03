777caaa949d4b4f96eb61fb41acf875c2605dda4
cayman444 <artursavinX@yandex.ru>
2025-11-03 14:06:54 +0300

    feat: add base url

--- src/app/router/config.tsx
+++ src/app/router/config.tsx
@@ -10,36 +10,39 @@ export const ROUTES_PATHS = {
   CREATE_PRODUCT: '/create-product',
 } as const;
 
-export const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        loader: () => redirect(ROUTES_PATHS.PRODUCTS),
-      },
-      {
-        path: ROUTES_PATHS.PRODUCTS,
-        element: <Products />,
-      },
-      {
-        path: ROUTES_PATHS.PRODUCTS_DETAILS,
-        element: <ProductDetails />,
-        loader: ({ params }) => {
-          const id = params.id;
-
-          if (id) {
-            store.dispatch(fetchProduct(id));
-          }
-
-          return null;
+export const router = createBrowserRouter(
+  [
+    {
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          loader: () => redirect(ROUTES_PATHS.PRODUCTS),
         },
-      },
-      {
-        path: ROUTES_PATHS.CREATE_PRODUCT,
-        element: <CreateProduct />,
-      },
-      { path: '*', element: <NotFound /> },
-    ],
-  },
-]);
+        {
+          path: ROUTES_PATHS.PRODUCTS,
+          element: <Products />,
+        },
+        {
+          path: ROUTES_PATHS.PRODUCTS_DETAILS,
+          element: <ProductDetails />,
+          loader: ({ params }) => {
+            const id = params.id;
+
+            if (id) {
+              store.dispatch(fetchProduct(id));
+            }
+
+            return null;
+          },
+        },
+        {
+          path: ROUTES_PATHS.CREATE_PRODUCT,
+          element: <CreateProduct />,
+        },
+        { path: '*', element: <NotFound /> },
+      ],
+    },
+  ],
+  { basename: '/test-task' }
+);
